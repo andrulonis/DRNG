6a6f296922fa1469bd78ccb9f11c21d4b1bbd9c5
andrulonis <48800417+andrulonis@users.noreply.github.com>
2022-08-24 01:47:41 +0200

    v0.9, everything works, need to update rest of json data

--- DRNG.py
+++ DRNG.py
@@ -1,5 +1,5 @@
 from operator import mod
-from random import randint, randrange
+from random import randint, randrange, sample
 from tkinter import *
 from PIL import ImageTk, Image
 import json
@@ -29,7 +29,7 @@ class Grenade:
         self.image = image
 
 class Loadout:
-    def __init__(self, primary, secondary, first_gadget, second_gadget, grenade, armor, pickaxe):
+    def __init__(self, primary, secondary, first_gadget, second_gadget, grenade, armor, pickaxe, passive_perks, active_perks):
         self.primary = primary
         self.secondary = secondary
         self.first_gadget = first_gadget
@@ -37,6 +37,8 @@ class Loadout:
         self.grenade = grenade
         self.armor = armor
         self.pickaxe = pickaxe
+        self.passive_perks = passive_perks
+        self.active_perks = active_perks
 
 def randomise_loadout(dwarf_class, data):
     random_primary = data[dwarf_class]["primaries"][randrange(len(data[dwarf_class]["primaries"]))]
@@ -52,11 +54,15 @@ def randomise_loadout(dwarf_class, data):
     random_grenade = randrange(len(data[dwarf_class]["grenades"]["names"]))
     grenade = Grenade(data[dwarf_class]["grenades"]["names"][random_grenade], data[dwarf_class]["grenades"]["images"][random_grenade])
 
-    armor = [randint(1,mod) for mod in ARMOR_MODS_PATTERN]
+    armor = [randint(1,mod) for mod in data["armor"]["mods_pattern"]]
 
-    pickaxe = [randint(1,mod) for mod in PICKAXE_MODS_PATTERN]
+    pickaxe = [randint(1,mod) for mod in data["pickaxe"]["mods_pattern"]]
 
-    return Loadout(primary_weapon, secondary_weapon, first_gadget, second_gadget, grenade, armor, pickaxe)
+    passive_perks = sample(data["passive_perks"], 3)
+
+    active_perks = sample(data["active_perks"], 2)
+
+    return Loadout(primary_weapon, secondary_weapon, first_gadget, second_gadget, grenade, armor, pickaxe, passive_perks, active_perks)
 
 def display_loadout(selected_class, data, root, loadout_labels):
     loadout = randomise_loadout(selected_class, data)
@@ -65,62 +71,71 @@ def display_loadout(selected_class, data, root, loadout_labels):
         label.grid_forget()
     loadout_labels.clear()
 
-    loadout_labels.append(primary_label := Label(root, text=loadout.primary.name + "\n" + loadout.primary.overclock + " " + " ".join(map(str, loadout.primary.mods_pattern))))
-    primary_label.grid(column=1, row=2, padx=10, pady=10)
+    loadout_frame = Frame(root)
+    loadout_frame.grid(column=0, row=2, columnspan=4)
+    loadout_frame.grid_columnconfigure((0, 1, 2, 3), weight = 1, uniform="uniform")
+
+    loadout_labels.append(primary_label := Label(loadout_frame, text=loadout.primary.name + "\n" + loadout.primary.overclock + " " + " ".join(map(str, loadout.primary.mods_pattern))))
+    primary_label.grid(column=0, row=2, padx=10, pady=10)
 
     global primary_image
     primary_image = ImageTk.PhotoImage(Image.open(loadout.primary.image))
-    loadout_labels.append(primary_image_label := Label(root, image=primary_image))
-    primary_image_label.grid(column=2, row=2, padx=10, pady=10)
+    loadout_labels.append(primary_image_label := Label(loadout_frame, image=primary_image))
+    primary_image_label.grid(column=1, row=2, padx=10, pady=10)
 
-    loadout_labels.append(secondary_label := Label(root, text=loadout.secondary.name + "\n" + loadout.secondary.overclock + " " + " ".join(map(str, loadout.secondary.mods_pattern))))
-    secondary_label.grid(column=1, row=3, padx=10, pady=10)
+    loadout_labels.append(secondary_label := Label(loadout_frame, text=loadout.secondary.name + "\n" + loadout.secondary.overclock + " " + " ".join(map(str, loadout.secondary.mods_pattern))))
+    secondary_label.grid(column=2, row=2, padx=10, pady=10)
 
     global secondary_image
     secondary_image = ImageTk.PhotoImage(Image.open(loadout.secondary.image))
-    loadout_labels.append(secondary_image_label := Label(root, image=secondary_image))
-    secondary_image_label.grid(column=2, row=3, padx=10, pady=10)
+    loadout_labels.append(secondary_image_label := Label(loadout_frame, image=secondary_image))
+    secondary_image_label.grid(column=3, row=2, padx=10, pady=10)
 
-    loadout_labels.append(first_gadget_label := Label(root, text=loadout.first_gadget.name + "\n" + " ".join(map(str, loadout.first_gadget.mods_pattern))))
-    first_gadget_label.grid(column=1, row=4, padx=10, pady=10)
+    loadout_labels.append(first_gadget_label := Label(loadout_frame, text=loadout.first_gadget.name + "\n" + " ".join(map(str, loadout.first_gadget.mods_pattern))))
+    first_gadget_label.grid(column=0, row=3, padx=10, pady=10)
 
     global first_gadget_image
     first_gadget_image = ImageTk.PhotoImage(Image.open(loadout.first_gadget.image))
-    loadout_labels.append(first_gadget_image_label := Label(root, image=first_gadget_image))
-    first_gadget_image_label.grid(column=2, row=4, padx=10, pady=10)
+    loadout_labels.append(first_gadget_image_label := Label(loadout_frame, image=first_gadget_image))
+    first_gadget_image_label.grid(column=1, row=3, padx=10, pady=10)
 
-    loadout_labels.append(second_gadget_label := Label(root, text=loadout.second_gadget.name + "\n" + " ".join(map(str, loadout.second_gadget.mods_pattern))))
-    second_gadget_label.grid(column=1, row=5, padx=10, pady=10)
+    loadout_labels.append(second_gadget_label := Label(loadout_frame, text=loadout.second_gadget.name + "\n" + " ".join(map(str, loadout.second_gadget.mods_pattern))))
+    second_gadget_label.grid(column=2, row=3, padx=10, pady=10)
     
     global second_gadget_image
     second_gadget_image = ImageTk.PhotoImage(Image.open(loadout.second_gadget.image))
-    loadout_labels.append(second_gadget_image_label := Label(root, image=second_gadget_image))
-    second_gadget_image_label.grid(column=2, row=5, padx=10, pady=10)
+    loadout_labels.append(second_gadget_image_label := Label(loadout_frame, image=second_gadget_image))
+    second_gadget_image_label.grid(column=3, row=3, padx=10, pady=10)
 
-    loadout_labels.append(grenade_label := Label(root, text=loadout.grenade.name))
-    grenade_label.grid(column=1, row=6, padx=10, pady=10)
+    loadout_labels.append(grenade_label := Label(loadout_frame, text=loadout.grenade.name))
+    grenade_label.grid(column=0, row=4, padx=10, pady=10)
 
     global grenade_image
     grenade_image = ImageTk.PhotoImage(Image.open(loadout.grenade.image))
-    loadout_labels.append(grenade_image_label := Label(root, image=grenade_image))
-    grenade_image_label.grid(column=2, row=6, padx=10, pady=10)
+    loadout_labels.append(grenade_image_label := Label(loadout_frame, image=grenade_image))
+    grenade_image_label.grid(column=1, row=4, padx=10, pady=10)
 
-    loadout_labels.append(pickaxe_label := Label(root, text= "Armor\n" + " ".join(map(str, loadout.armor))))
-    pickaxe_label.grid(column=1, row=7, padx=10, pady=10)
+    loadout_labels.append(pickaxe_label := Label(loadout_frame, text= "Armor\n" + " ".join(map(str, loadout.armor))))
+    pickaxe_label.grid(column=2, row=4, padx=10, pady=10)
 
     global armor_image
-    armor_image = ImageTk.PhotoImage(Image.open(ARMOR_IMAGE))
-    loadout_labels.append(armor_image_label := Label(root, image=armor_image))
-    armor_image_label.grid(column=2, row=7, padx=10, pady=10)
+    armor_image = ImageTk.PhotoImage(Image.open(data["armor"]["image"]))
+    loadout_labels.append(armor_image_label := Label(loadout_frame, image=armor_image))
+    armor_image_label.grid(column=3, row=4, padx=10, pady=10)
 
-    loadout_labels.append(pickaxe_label := Label(root, text= "Pickaxe\n" + " ".join(map(str, loadout.pickaxe))))
-    pickaxe_label.grid(column=1, row=8, padx=10, pady=10)
+    loadout_labels.append(pickaxe_label := Label(loadout_frame, text= "Pickaxe\n" + " ".join(map(str, loadout.pickaxe))))
+    pickaxe_label.grid(column=0, row=5, padx=10, pady=10)
 
-    #TODO: find pickaxe icon
-    # global pickaxe_image
-    # pickaxe_image = ImageTk.PhotoImage(Image.open(PICKAXE_IMAGE))
-    # loadout_labels.append(pickaxe_image_label := Label(root, image=pickaxe_image))
-    # pickaxe_image_label.grid(column=2, row=7, padx=10, pady=10)
+    global pickaxe_image
+    pickaxe_image = ImageTk.PhotoImage(Image.open(data["pickaxe"]["image"]))
+    loadout_labels.append(pickaxe_image_label := Label(loadout_frame, image=pickaxe_image))
+    pickaxe_image_label.grid(column=1, row=5, padx=10, pady=10)
+
+    loadout_labels.append(passive_perks_label := Label(loadout_frame, text= "Passive perks:\n" + "\n".join(map(str, loadout.passive_perks))))
+    passive_perks_label.grid(column=2, row=5, padx=10, pady=10)
+
+    loadout_labels.append(active_perks_label := Label(loadout_frame, text= "Active perks:\n" + "\n".join(map(str, loadout.active_perks))))
+    active_perks_label.grid(column=3, row=5, padx=10, pady=10)
 
 def main():
     fp = open("data.json")
@@ -129,7 +144,7 @@ def main():
     root = Tk()
     root.title("DRNG")
     root.resizable(False, False)
-    root.geometry("800x1000")
+    root.geometry("1000x750")
     root.grid()
     root.grid_columnconfigure((0, 1, 2, 3), weight = 1)
     selected_class = StringVar(root, "gunner")
