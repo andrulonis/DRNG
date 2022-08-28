e2034824624fd21e2dca275ec9ceb56684f41fb4
andrulonis <48800417+andrulonis@users.noreply.github.com>
2022-08-28 22:43:23 +0200

    exe working as standalone

--- DRNG.py
+++ DRNG.py
@@ -1,19 +1,14 @@
-from operator import mod
 from random import randint, randrange, sample
 from tkinter import *
-from PIL import ImageTk, Image
+from PIL import ImageTk
 import json
-
-ARMOR_MODS_PATTERN = [3, 2, 1, 3]
-PICKAXE_MODS_PATTERN = [1, 3]
-ARMOR_IMAGE = "./resources/images/armor.png"
-PICKAXE_IMAGE = "./resources/images/pickaxe.png"
-DWARF_CLASSES = ["gunner", "scout", "engineer", "driller"]
+import sys
+import os
 
 class Main_Application:
     def __init__(self, root, data):
         root.title("DRNG")
-        icon = ImageTk.PhotoImage(file="./resources/icons/dice.png")
+        icon = ImageTk.PhotoImage(file=resource_path("./resources/icons/dice.ico"))
         root.iconphoto(False, icon)
         root.resizable(False, False)
         root.geometry("1000x700")
@@ -23,10 +18,10 @@ class Main_Application:
         self.selected_class = StringVar(root)
         buttons_frame = Frame(root)
         buttons_frame.grid(column=0, row=0, columnspan=4)
-        self.gunner_icon = PhotoImage(file="./resources/icons/gunner_icon.png")
-        self.scout_icon = PhotoImage(file="./resources/icons/scout_icon.png")
-        self.engineer_icon = PhotoImage(file="./resources/icons/engineer_icon.png")
-        self.driller_icon = PhotoImage(file="./resources/icons/driller_icon.png")
+        self.gunner_icon = PhotoImage(file=resource_path("./resources/icons/gunner_icon.png"))
+        self.scout_icon = PhotoImage(file=resource_path("./resources/icons/scout_icon.png"))
+        self.engineer_icon = PhotoImage(file=resource_path("./resources/icons/engineer_icon.png"))
+        self.driller_icon = PhotoImage(file=resource_path("./resources/icons/driller_icon.png"))
         Radiobutton(buttons_frame, bg="green", value="gunner", image=self.gunner_icon, indicatoron=0, variable=self.selected_class).grid(column=0, row=0, padx=50, pady=20)
         Radiobutton(buttons_frame, bg="blue", value="scout", image=self.scout_icon, indicatoron=0, variable=self.selected_class).grid(column=1, row=0, padx=50, pady=20)
         Radiobutton(buttons_frame, bg="red", value="engineer", image=self.engineer_icon, indicatoron=0, variable=self.selected_class).grid(column=2, row=0, padx=50, pady=20)
@@ -53,49 +48,49 @@ class Main_Application:
         loadout_labels.append(primary_label := Label(loadout_frame, text=loadout.primary.name + "\n" + loadout.primary.overclock + "\n" + " ".join(map(str, loadout.primary.mods_pattern))))
         primary_label.grid(column=0, row=2, padx=10, pady=10)
 
-        self.primary_image = ImageTk.PhotoImage(Image.open(loadout.primary.image))
+        self.primary_image = ImageTk.PhotoImage(file=resource_path(loadout.primary.image))
         loadout_labels.append(primary_image_label := Label(loadout_frame, image=self.primary_image))
         primary_image_label.grid(column=1, row=2, padx=10, pady=10)
 
         loadout_labels.append(secondary_label := Label(loadout_frame, text=loadout.secondary.name + "\n" + loadout.secondary.overclock + "\n" + " ".join(map(str, loadout.secondary.mods_pattern))))
         secondary_label.grid(column=2, row=2, padx=10, pady=10)
 
-        self.secondary_image = ImageTk.PhotoImage(Image.open(loadout.secondary.image))
+        self.secondary_image = ImageTk.PhotoImage(file=resource_path(loadout.secondary.image))
         loadout_labels.append(secondary_image_label := Label(loadout_frame, image=self.secondary_image))
         secondary_image_label.grid(column=3, row=2, padx=10, pady=10)
 
         loadout_labels.append(first_gadget_label := Label(loadout_frame, text=loadout.first_gadget.name + "\n" + " ".join(map(str, loadout.first_gadget.mods_pattern))))
         first_gadget_label.grid(column=0, row=3, padx=10, pady=10)
 
-        self.first_gadget_image = ImageTk.PhotoImage(Image.open(loadout.first_gadget.image))
+        self.first_gadget_image = ImageTk.PhotoImage(file=resource_path(loadout.first_gadget.image))
         loadout_labels.append(first_gadget_image_label := Label(loadout_frame, image=self.first_gadget_image))
         first_gadget_image_label.grid(column=1, row=3, padx=10, pady=10)
 
         loadout_labels.append(second_gadget_label := Label(loadout_frame, text=loadout.second_gadget.name + "\n" + " ".join(map(str, loadout.second_gadget.mods_pattern))))
         second_gadget_label.grid(column=2, row=3, padx=10, pady=10)
         
-        self.second_gadget_image = ImageTk.PhotoImage(Image.open(loadout.second_gadget.image))
+        self.second_gadget_image = ImageTk.PhotoImage(file=resource_path(loadout.second_gadget.image))
         loadout_labels.append(second_gadget_image_label := Label(loadout_frame, image=self.second_gadget_image))
         second_gadget_image_label.grid(column=3, row=3, padx=10, pady=10)
 
         loadout_labels.append(grenade_label := Label(loadout_frame, text=loadout.grenade.name))
         grenade_label.grid(column=0, row=4, padx=10, pady=10)
 
-        self.grenade_image = ImageTk.PhotoImage(Image.open(loadout.grenade.image))
+        self.grenade_image = ImageTk.PhotoImage(file=resource_path(loadout.grenade.image))
         loadout_labels.append(grenade_image_label := Label(loadout_frame, image=self.grenade_image))
         grenade_image_label.grid(column=1, row=4, padx=10, pady=10)
 
         loadout_labels.append(pickaxe_label := Label(loadout_frame, text= "Armor Rig\n" + " ".join(map(str, loadout.armor))))
         pickaxe_label.grid(column=2, row=4, padx=10, pady=10)
 
-        self.armor_image = ImageTk.PhotoImage(Image.open(data["armor"]["image"]))
+        self.armor_image = ImageTk.PhotoImage(file=resource_path(data["armor"]["image"]))
         loadout_labels.append(armor_image_label := Label(loadout_frame, image=self.armor_image))
         armor_image_label.grid(column=3, row=4, padx=10, pady=10)
 
         loadout_labels.append(pickaxe_label := Label(loadout_frame, text= "Pickaxe\n" + " ".join(map(str, loadout.pickaxe))))
         pickaxe_label.grid(column=0, row=5, padx=10, pady=10)
 
-        self.pickaxe_image = ImageTk.PhotoImage(Image.open(data["pickaxe"]["image"]))
+        self.pickaxe_image = ImageTk.PhotoImage(file=resource_path(data["pickaxe"]["image"]))
         loadout_labels.append(pickaxe_image_label := Label(loadout_frame, image=self.pickaxe_image))
         pickaxe_image_label.grid(column=1, row=5, padx=10, pady=10)
 
@@ -159,8 +154,23 @@ def randomise_loadout(dwarf_class, data):
 
     return Loadout(primary_weapon, secondary_weapon, first_gadget, second_gadget, grenade, armor, pickaxe, passive_perks, active_perks)
 
+def resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+ARMOR_MODS_PATTERN = [3, 2, 1, 3]
+PICKAXE_MODS_PATTERN = [1, 3]
+ARMOR_IMAGE = resource_path("./resources/images/armor.png")
+PICKAXE_IMAGE = resource_path("./resources/images/pickaxe.png")
+DWARF_CLASSES = ["gunner", "scout", "engineer", "driller"]
+
 def main():
-    fp = open("data.json")
+    fp = open(resource_path("./data.json"))
     data = json.load(fp)
 
     root = Tk()
